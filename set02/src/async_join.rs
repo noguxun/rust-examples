a7e07d3c4627ab5c0fb565f75d43bf6b29d34a47
Xun Gu <noguxun@gmaifl.com>
2020-05-06 14:33:44 +0900

    add tokio samples

---
+++ set02/src/async_join.rs
@@ -0,0 +1,86 @@
+
+#[derive(Debug)]
+struct Data {
+    resp: String,
+}
+
+async fn do_stuff1() -> Data {
+	println!("do_stuff1 start");
+	let client = reqwest::Client::new();
+
+	let res = client
+			.post("http://httpbin.org/post")
+			.body("stuff1 body")
+			.header("abc", "def")
+			.send()
+			.await
+			.unwrap();
+
+	let resp_str = res.text().await.unwrap();
+
+	println!("do_stuff1 end");
+	Data { resp: resp_str }
+}
+
+async fn do_stuff2() -> Data {
+	println!("do_stuff2 start");
+	let client = reqwest::Client::new();
+
+	let res = client
+			.post("http://httpbin.org/post")
+			.body("stuff2 body")
+			.header("123", "456")
+			.send()
+			.await
+			.unwrap();
+
+	let resp_str = res.text().await.unwrap();
+	println!("do_stuff2 end");
+
+	Data { resp: resp_str }
+}
+
+async fn do_stuff3(header: &str, value: String) -> Data {
+	println!("dull_job start {}" , value);
+	let client = reqwest::Client::new();
+
+	let res = client
+			.post("http://httpbin.org/post")
+			.body("the exact body that is sent")
+			.header(header, value.clone())
+			.send()
+			.await
+			.unwrap();
+
+	let resp_str = res.text().await.unwrap();
+	println!("dull_job end {}", value);
+
+	Data { resp: resp_str }
+}
+
+pub async fn run() {
+
+	// https://docs.rs/tokio/0.2.18/tokio/macro.join.html
+	println!("Test tokio::join");
+	let (first, second) = tokio::join!(do_stuff1(), do_stuff2());
+	
+	println!("first {}", first.resp);
+	println!("second {}", second.resp);
+
+
+	println!("Test tokio::spawn, await");
+	let mut handle_vec = Vec::new();
+
+	for i in 0..10 {
+			let value = format!("{}",i);
+			let handle = tokio::task::spawn(do_stuff3("xgu-debug", value));
+			handle_vec.push(handle);
+	}
+	println!("all jobs are spanwed");
+
+	while let Some(handle) = handle_vec.pop() {
+			let data = handle.await.unwrap();
+			println!("got {}", data.resp);
+	}
+
+}
---
+++ set02/src/main.rs
@@ -0,0 +1,12 @@
+mod async_join;
+
+
+
+#[tokio::main]
+async fn main() {
+    println!("====================");
+    println!(" Asnyc Join");
+    println!("====================");
+    async_join::run().await;
+    println!("\n");
+}
